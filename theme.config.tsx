7455572ed431b7922a23cbee7ba05d2858196b66
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-02-19 00:30:45 +0900

    feat : Nextra Docs 적용

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <strong>DongWook's 개발 공부</strong>,
+  project: {
+    link: "https://github.com/Lee-Dongwook/Frontend_Study",
+  },
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    text: "꾸준한 기록과 학습을",
+  },
+};
+
+export default config;
