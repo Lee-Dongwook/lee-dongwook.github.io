8ee00c31d3680db75a56de1090a228431608fabc
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-02-19 23:59:30 +0900

    ci : gh-pages 구성

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const withNextra = require('nextra')({
     themeConfig: './theme.config.tsx',
   })
   
-  module.exports = withNextra()
+  module.exports = {
+    ...withNextra(),
+    output: 'export',
+    images: {
+      unoptimized: true,
+    }
+  }
