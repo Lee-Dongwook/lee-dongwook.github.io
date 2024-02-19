07a7f0b05fcf9bfe0994f82f75ea258646be1df9
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-02-19 21:19:40 +0900

    feat : Meta OG(오픈그래프) 적용

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,8 +3,21 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <strong>DongWook's Develop Study!</strong>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Dongwook_Develop_Study" />
+      <meta
+        property="og:description"
+        content="Nextra Docs Template Based Next Project"
+      />
+    </>
+  ),
   project: {
     link: "https://github.com/Lee-Dongwook/Frontend_Study",
+  },
+  toc: {
+    backToTop: true,
   },
   docsRepositoryBase: "https://github.com/Lee-Dongwook/Frontend_Study",
   footer: {
