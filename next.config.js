ddce65e1ce3e4d5ca6d533c2ae03ac99cfbebeb4
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-02-29 19:15:33 +0900

    feat : Next.JS SVG 파일 추가

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = withNextra({
     images: {
         unoptimized: true
     },
-})
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+        });
+
+        return config;
+    },
+});
--- theme.config.js
+++ theme.config.js
@@ -1,3 +1,6 @@
+import Github from './assets/github.svg';
+import Velog from './assets/velog.svg';
+
 export default {
   comments: (
     <>
@@ -5,50 +8,30 @@ export default {
     </div>
     </>
   ),
-  footer: (<div className="my-24  border-t-2  mx-auto flex items-center sm:flex-row flex-col">
-      <p className="text-sm text-gray-500">© By Rajdeep singh  —
-        <a href="https://twitter.com/Official_R_deep" className="text-gray-600" rel="noopener noreferrer" target="_blank">@Official_R_deep</a>
-      </p>
-      <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-        
-        <a target="_blank" href="https://www.facebook.com/officialrajdeepsingh" className="text-gray-500">
-          <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-          </svg>
-        </a>
-        <a target="_blank" href="https://twitter.com/Official_R_deep" className="ml-3 text-gray-500">
-          <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-          </svg>
-        </a>
-        <a target="_blank" href="https://www.instagram.com/officialrajdeepsingh/" className="ml-3 text-gray-500">
-          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-          </svg>
-        </a>
-        <a target="_blank" href="https://www.linkedin.com/in/officalrajdeepsingh/" className="ml-3 text-gray-500">
-          <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
-            <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-            <circle cx="4" cy="4" r="2" stroke="none"></circle>
-          </svg>
-        </a>
-      </span>
-    </div>
-
-  ),
+  footer: (<footer className="border-t-2 mx-auto flex">
+  <p className="text-sm text-gray-500">© By Lee-Dongwook - 2024 </p>
+  <span className='inline-flex mt-3 justify-center'>
+    <a target="_blank" href="https://www.github.com/Lee-Dongwook">
+      <Github />
+    </a>
+  </span>
+  <span className='inline-flex mt-3 justify-center'>
+    <a target="_blank" href="https://www.github.com/Lee-Dongwook">
+      <Velog />
+    </a>
+  </span>
+  <span className="inline-flex mt-4 justify-center">
+    <a target="_blank" href="https://www.linkedin.com/in/dong-wook-lee-1095112a0/" className="ml-3 text-gray-500">
+      <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+        <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+        <circle cx="4" cy="4" r="2" stroke="none"></circle>
+      </svg>
+    </a>
+  </span>
+</footer> ),
   unstable_faviconGlyph: "👋",
   navs: [
-    {
-      url: "/feed.xml",
-      name: "RSS",
-      "newWindow": true
-    },
-    {
-      url: "https://www.linkedin.com/in/officalrajdeepsingh/",
-      name: "Github",
-      "newWindow": true
-    },
+   
   ],
   readMore: 'Read Now',
   postFooter: (
