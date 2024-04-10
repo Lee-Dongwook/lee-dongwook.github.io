b90ec53d0fd72890e029ab73e80d28e6609234f2
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-04-10 15:44:43 +0900

    chore : docs template

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra({ 
-    output: 'export',
-    images: {
-        unoptimized: true
-    },
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ["@svgr/webpack"],
-        });
+const withNextra = require("nextra")("nextra-theme-docs", "./theme.config.js");
+module.exports = withNextra({
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-        return config;
-    },
+    return config;
+  },
 });
