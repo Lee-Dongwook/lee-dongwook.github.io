e45159669708bde2a15150e8c39bfedf607615d0
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-11-20 11:42:26 +0900

    init

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const withNextra = require("nextra")("nextra-theme-blog", "./theme.config.js");
+module.exports = withNextra({
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/nextra-blog",
+});
